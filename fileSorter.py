4334a093459cb5e5093014546d1de49b04b569a3
Bartosz Leśniewski <blesniewski@outlook.com>
2022-11-18 22:23:46 +0100

    Whole sorting process works

--- fileSorter.py
+++ fileSorter.py
@@ -8,6 +8,7 @@ class FileSorter:
         self.tape2 = Tape("tape2.txt")
         self.tape3 = Tape("tape3.txt")
         self.next_tape = self.tape2
+        self.file_sorted = False
 
     def copy_initial_file(self):
         with open(self.initial_filename, "r") as initial_file, open("tape1.txt", "w") as tape_file:
@@ -16,8 +17,11 @@ class FileSorter:
 
     def sort(self):
         self.copy_initial_file()
-        self.distribute()
-        self.merging()
+        while not self.file_sorted:
+            self.distribute()
+            self.merge()
+            self.next_tape = self.tape2
+
         pass
 
     def distribute(self):
@@ -44,10 +48,10 @@ class FileSorter:
         self.tape1.clear()
         tape2_record, tape2_last_record = None, None
         tape3_record, tape3_last_record = None, None
-        tape2_runs_counter, tape3_runs_counter = 1, 1
+        tape2_runs_counter, tape3_runs_counter = 0, 0
 
         while True:
-            # Already saved record is marked as None.
+            # Already merged record is marked as None.
             # If record is not saved yet, it will be used in the next iteration.
             if tape2_runs_counter == 7:
                 pass
@@ -55,11 +59,11 @@ class FileSorter:
             tape2_record = self.tape2.fetch_record() if tape2_record is None else tape2_record
             tape3_record = self.tape3.fetch_record() if tape3_record is None else tape3_record
 
-            if tape2_record is not None and tape2_last_record > tape2_record:
+            if (tape2_record is not None and tape2_last_record > tape2_record) or (tape2_record is None):
                 tape2_runs_counter += 1
                 tape2_last_record = None  # to avoid incrementing counter in the next iteration under the same condition
 
-            if tape3_record is not None and tape3_last_record > tape3_record:
+            if (tape3_record is not None and tape3_last_record > tape3_record) or (tape2_record is None):
                 tape3_runs_counter += 1
                 tape3_last_record = None
 
@@ -96,3 +100,8 @@ class FileSorter:
 
         if not self.tape1.block.is_empty():
             self.tape1.save_block_to_file()
+
+        self.tape2.clear()
+        self.tape3.clear()
+        if tape2_runs_counter + tape3_runs_counter <= 2:
+            self.file_sorted = True
